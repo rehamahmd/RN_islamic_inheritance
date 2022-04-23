e7b7a3b88e330f5f8fda116feb17699a63a6c7c7
rehamahmad <reham_ahmad94@yahoo.com>
2022-04-24 01:22:23 +0200

    init & icon & splash screen

---
+++ src/navigator.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RNBootSplash from 'react-native-bootsplash';
+import HomeScreen from './screens/home_screen';
+import DetailScreen from './screens/details_screen';
+
+
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer onReady={() => RNBootSplash.hide()}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
